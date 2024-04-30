e355e777d01e5446bf81e3bdce40808022bf6698
RicardoAzevedo <ricardoazevedo1911@gmail.com>
2024-04-30 00:11:27 -0300

    inicialização

---
+++ src/main/java/edu/ricardooazevedo/java/tiposvariaveis/TiposVariaveis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.ricardooazevedo.java.tiposvariaveis;
+
+/**
+ *
+ * @author ricar
+ */
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        
+    }
+}
