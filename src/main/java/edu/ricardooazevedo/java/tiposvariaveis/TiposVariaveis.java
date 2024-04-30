552235cdaeb19476440b49fdf38db38408f83373
RicardoAzevedo <ricardoazevedo1911@gmail.com>
2024-04-30 00:38:02 -0300

    tipos primitivos

--- src/main/java/edu/ricardooazevedo/java/tiposvariaveis/TiposVariaveis.java
+++ src/main/java/edu/ricardooazevedo/java/tiposvariaveis/TiposVariaveis.java
@@ -1,6 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package edu.ricardooazevedo.java.tiposvariaveis;
 
@@ -9,8 +6,20 @@ package edu.ricardooazevedo.java.tiposvariaveis;
  * @author ricar
  */
 public class TiposVariaveis {
-
+    // revisão sobre os tipos primitivos do java:
     public static void main(String[] args) {
+        
+        byte idade = 123;
+        
+        short ano = 2021;
+        
+        int cep = 557000147;// se começar com zero, tera que ser String.
+        
+        long cpf = 15612323403L; // se começar com zero, tera que ser String.
+        
+        float pi = 3.14F;
+        
+        double salarioMinimo = 1412.22;
         
     }
 }
